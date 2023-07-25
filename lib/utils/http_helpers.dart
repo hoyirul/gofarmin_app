606ef4d33c490e7c0fc710792daa8e6c39eaef2a
hoyirul <irulgraspama621@gmail.com>
2023-07-25 22:38:10 +0700

    Fixed url API

--- lib/utils/http_helpers.dart
+++ lib/utils/http_helpers.dart
@@ -1,9 +1,9 @@
 class HttpHelper {
-  final String url = 'http://192.168.1.6:8000';
+  final String url = 'https://services.gofarmin.com';
   final String development = 'http://192.168.1.6:8000/api';
-  final String production = 'https://your-domain:8000/api';
+  final String production = 'https://services.gofarmin.com/api';
 
   String getUri(String entpoints) {
-    return development + entpoints;
+    return production + entpoints;
   }
 }
