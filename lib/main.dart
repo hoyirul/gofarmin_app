6c5be845759cfb5be9922d395bc4719fdef85f85
hoyirul <irulgraspama621@gmail.com>
2023-07-31 22:38:52 +0700

    Fixed Main

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:gofarmin_app/screens/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
 
+// Main dart
 void main() {
   runApp(const GetMaterialApp(
     debugShowCheckedModeBanner: false,
